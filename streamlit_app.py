6ba4aa1c5da925312fb5ecc0b78b04e94a985e02
alibama <adp6j@virginia.edu>
2020-11-17 18:37:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
-import geopandas as gpd
+import geopandas
 import datetime
 """
 # Welcome to The Cville Tree Commission Neighborhood Tree App!
