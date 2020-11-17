832a40a9ef8b2e3dda4154f3a99f370d9c6c65f3
alibama <adp6j@virginia.edu>
2020-11-17 18:35:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,22 +4,34 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
-
+import geopandas as gpd
+import datetime
 """
-# Welcome to Streamlit!
+# Welcome to The Cville Tree Commission Neighborhood Tree App!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
+cvilletrees = "https://opendata.arcgis.com/datasets/e7c856379492408e9543a25d684b8311_79.geojson"
+treesdf = gd.read_file(cvilletrees)   
 
 
+
+today = datetime.date.today()
+tomorrow = today + datetime.timedelta(days=1)
+start_date = st.date_input('Start date', today)
+end_date = st.date_input('End date', tomorrow)
+if start_date < end_date:
+    st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+else:
+    st.error('Error: End date must fall after start date.')
+    
 
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -78.4],
+    np.random.randn(1000, 2) / [50, 50] + [38.76, -78.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
