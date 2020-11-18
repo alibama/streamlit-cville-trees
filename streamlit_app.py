cd5e579922ef9abd6e581cafbedd199e1ab38142
alibama <adp6j@virginia.edu>
2020-11-17 21:32:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ forums](https://discuss.streamlit.io).
 r = requests.get('https://opendata.arcgis.com/datasets/e7c856379492408e9543a25d684b8311_79.geojson').json()
 
 df = pd.DataFrame.from_dict(r)
-df
 
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
