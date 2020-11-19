806d118ecae2045b66bbe8d6cac5c162e61a2a27
alibama <adp6j@virginia.edu>
2020-11-18 21:37:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ data_top = df.head()
 data_top  
 
 
-test = gpd.read_file('https://github.com/alibama/streamlit-example/raw/master/planning_area_09_03_2020.shp')
+test = gpd.read_file("https://github.com/alibama/streamlit-example/raw/master/planning_area_09_03_2020.shp")
 inline_data = alt.InlineData(test.to_json())
 chart = alt.Chart(inline_data).mark_geoshape()
 st.altair_chart(chart)
